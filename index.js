28c4c81e49c6b7a9e944923ae43dfbaf0deb6a77
Khandaker Samin Yeasar <138380913+KhandakerSamin@users.noreply.github.com>
2023-11-28 02:41:48 +0600

    Some status update api creatd

--- index.js
+++ index.js
@@ -87,6 +87,7 @@ async function run() {
         });
 
         //* Make a User Delivery Man
+        
         app.patch('/users/delivery-man/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) };
